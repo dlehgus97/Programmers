26b42a2c39e0cc0ecbaac404de7d7704b89ec35d
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2021-11-09 01:51:14 +0900

    Create 2016년.cpp

---
+++ C++/2016년.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string solution(int a, int b) {
+    int month[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    string day[7] = { "THU", "FRI", "SAT" ,"SUN", "MON","TUE", "WED" };
+    int sum = 0;
+    for (int i = 0; i < a - 1; i++) {
+        sum += month[i];
+    }
+    return day[(sum + b) % 7];
+
+}
